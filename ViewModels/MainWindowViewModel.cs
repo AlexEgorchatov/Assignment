f8175f824d9153b80340f6dd1125c48573b75072
Alex <alex.exorchatov@gmail.com>
2021-03-21 17:02:19 -0700

    Added basic TabContorl

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,8 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
-using System.Collections.Generic;
-using System.Windows.Input;
+using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Assignment.ViewModels
 {
@@ -10,30 +10,33 @@ namespace Assignment.ViewModels
     {
         #region Private Fields
 
-        private List<string> _tabs;
+        private DelegateCommand _addTabCommand;
+        private double _headerWidth = 100;
+        private ObservableCollection<TabViewModel> _tabs;
+        private const double _headerMargin = 12;
 
         #endregion
 
         #region Public Properties
 
-        public List<string> Tabs
-        {
-            get { return _tabs; }
-            set { SetProperty(ref _tabs, value); }
-        }
+        public Thickness ButtonAddMargin => new Thickness(Tabs.Count * (_headerWidth + _headerMargin) + 10, 0, 0, 0);
 
-        private DelegateCommand<MouseButtonEventArgs> _dragCommand;
-        public DelegateCommand<MouseButtonEventArgs> DragCommand
+        public DelegateCommand AddTabCommand
         {
             get
             {
-                return _dragCommand ?? (_dragCommand = new DelegateCommand<MouseButtonEventArgs>((e) =>
+                return _addTabCommand ?? (_addTabCommand = new DelegateCommand(() =>
                 {
-                    
+                    Tabs.Add(new TabViewModel(_headerWidth));
                 }));
             }
         }
 
+        public ObservableCollection<TabViewModel> Tabs
+        {
+            get { return _tabs; }
+            set { SetProperty(ref _tabs, value); }
+        }
 
         #endregion
 
@@ -41,7 +44,17 @@ namespace Assignment.ViewModels
 
         public MainWindowViewModel()
         {
-            Tabs = new List<string>() { "New Tab", "Tab1" };
+            Tabs = new ObservableCollection<TabViewModel>() { new TabViewModel(_headerWidth) };
+            Tabs.CollectionChanged += OnTabsCollectionChanged;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void OnTabsCollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            RaisePropertyChanged(nameof(ButtonAddMargin));
         }
 
         #endregion
---
+++ ViewModels/TabViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Prism.Mvvm;
+
+namespace Assignment.ViewModels
+{
+    public class TabViewModel : BindableBase
+    {
+        #region Private Fields
+
+        private string _header;
+        private double _headerWidth;
+
+        #endregion
+
+        #region Public Properties
+
+        public string Header
+        {
+            get { return _header; }
+            set { SetProperty(ref _header, value); }
+        }
+
+        public double HeaderWidth
+        {
+            get { return _headerWidth; }
+            set { SetProperty(ref _headerWidth, value); }
+        }
+
+        #endregion
+
+        #region Public Constructors
+
+        public TabViewModel(double headerWidth)
+        {
+            Header = "New Tab";
+            HeaderWidth = headerWidth;
+        }
+
+        #endregion
+    }
+}
